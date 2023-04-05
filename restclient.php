43bb8c2323caa408750eb8f23f7ed0df79966ffc
ggeweb <129991793+ggeweb@users.noreply.github.com>
2023-04-05 17:33:08 -0300

    Update restclient.php

--- restclient.php
+++ restclient.php
@@ -6,6 +6,7 @@ namespace GGEWeb;
  * PHP REST Client
  * https://github.com/tcdent/php-restclient
  * (c) 2013-2022 Travis Dent <tcdent@gmail.com>
+ * Fork modifications 2023: Marcos Souza Filho <marcos@ggeweb.com.br>
  */
 
 
